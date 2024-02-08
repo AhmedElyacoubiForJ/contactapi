3ee7cdc4c02d278e05fea028611bffc22599b2b1
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-02-08 18:02:52 +0100

    added contact entity, repository and service

---
+++ src/main/java/edu/yacoubi/contactapi/constant/Constant.java
@@ -0,0 +1,6 @@
+package edu.yacoubi.contactapi.constant;
+
+public class Constant {
+    public static final String PHOTO_DIRECTORY = System.getProperty("user.home") +
+            "/downloads/uploads";
+}
